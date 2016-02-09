04b5b7977f10f0c3f76ecee95c67077dc499aaa5
Joe-noh <goflb.jh@gmail.com>
2016-02-09 22:07:36 +0900

    echo server works

--- lib/work_with_ruby/echo_server.ex
+++ lib/work_with_ruby/echo_server.ex
@@ -2,11 +2,19 @@ defmodule WorkWithRuby.EchoServer do
   use GenServer
 
   def start_link do
-    GenServer.start_link(__MODULE__, [])
+    GenServer.start_link(__MODULE__, [], name: __MODULE__)
+  end
+
+  def echo(string) do
+    GenServer.call(__MODULE__, {:echo, string})
   end
 
   def init(_) do
-    IO.puts "#{__MODULE__} started"
     {:ok, []}
+  end
+
+  def handle_call({:echo, string}, _from, state) when is_binary(string) do
+    IO.puts "Got \"#{string}\""
+    {:reply, string, state}
   end
 end
