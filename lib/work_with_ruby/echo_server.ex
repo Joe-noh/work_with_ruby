d3eb485b0d5472754122596234f2b571c72b7350
Joe-noh <goflb.jh@gmail.com>
2016-02-10 00:56:19 +0900

    work with ruby!

--- lib/work_with_ruby/echo_server.ex
+++ lib/work_with_ruby/echo_server.ex
@@ -5,16 +5,33 @@ defmodule WorkWithRuby.EchoServer do
     GenServer.start_link(__MODULE__, [], name: __MODULE__)
   end
 
-  def echo(string) do
-    GenServer.call(__MODULE__, {:echo, string})
+  def hello do
+    GenServer.call(__MODULE__, {:echo, "Hello\n"})
   end
 
   def init(_) do
-    {:ok, []}
+    Process.flag(:trap_exit, true)
+
+    rb_file = Path.join(:code.priv_dir(:work_with_ruby), "echo_server.rb")
+    pid = Port.open({:spawn_executable, rb_file}, [:binary, :stream])
+
+    {:ok, pid}
   end
 
   def handle_call({:echo, string}, _from, state) when is_binary(string) do
     IO.puts "Got \"#{string}\""
+
+    Port.command(state, string)
+
     {:reply, string, state}
+  end
+
+  def handle_info({:EXIT, _, :normal}, state) do
+    {:stop, :ok, state}
+  end
+
+  def handle_info(other, state) do
+    IO.inspect other
+    {:noreply, state}
   end
 end
---
+++ priv/echo_server.rb
@@ -0,0 +1,6 @@
+#!/usr/bin/env ruby
+
+while true
+  puts gets
+  $stdout.flush
+end
